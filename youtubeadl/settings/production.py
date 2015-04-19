f7b67cd68bfd63b81c57ea86ac28ceaca05be1e8
jonathan <jonathan@calazan.com>
2015-04-18 21:37:09 -0500

    Removed MEDIA_ROOT from production file.

--- youtubeadl/settings/production.py
+++ youtubeadl/settings/production.py
@@ -6,9 +6,6 @@ DEBUG = False
 ALLOWED_HOSTS = ['.youtubeadl.com']
 
 
-MEDIA_ROOT = '/webapps/youtubeadl/media/'
-
-
 # Use the cached template loader so template is compiled once and read from
 # memory instead of reading from disk on each load.
 TEMPLATES[0]['OPTIONS']['loaders'] = [
