ca941d2af21a51b503fd1305a168c9f2a5d31e61
jonathan <jonathan@calazan.com>
2015-04-19 18:18:34 -0500

    Added flower for celery monitoring, added stacked inline in admin>

--- youtubeadl/apps/downloader/admin.py
+++ youtubeadl/apps/downloader/admin.py
@@ -4,10 +4,16 @@ from django.utils.text import Truncator
 from youtubeadl.apps.downloader import models
 
 
+class ActivityLogInline(admin.TabularInline):
+    model = models.ActivityLog
+    readonly_fields = ('created',)
+
+
 @admin.register(models.ActivityLog)
 class ActivityLogAdmin(admin.ModelAdmin):
     list_display = (
         'video',
+        'video_title',
         'client_ip',
         'action',
         'created',
@@ -23,8 +29,17 @@ class ActivityLogAdmin(admin.ModelAdmin):
 class VideoAdmin(admin.ModelAdmin):
     list_display = (
         'youtube_id',
+        'title',
         'audio_filename',
         'duration',
         'download_count',
-        'last_download_date'
+        'last_download_date',
     )
+    list_filter = ('last_download_date',)
+    search_fields = (
+        'youtube_id',
+        'title',
+        'audio_filename',
+    )
+
+    inlines = [ActivityLogInline]
