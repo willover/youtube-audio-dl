bd544ae120b9a4c51a747e2cb565f527d211ee04
jonathan <jonathan@calazan.com>
2015-04-18 21:44:07 -0500

    Put share buttons in a panel.

--- scripts/deploy_youtubeadl.sh
+++ scripts/deploy_youtubeadl.sh
@@ -2,5 +2,5 @@
 
 cd ../ansible
 
-ansible-playbook -i production ../ansible/site.yml --tags="deploy" --ask-vault-pass
+ansible-playbook -i production ../ansible/site.yml --tags="deploy,nginx,virtualenv" --ask-vault-pass
 #ansible-playbook -i production ../ansible/site.yml --ask-vault-pass
