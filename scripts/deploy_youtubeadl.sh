fc9244fb92925ed1e5b6ff9d67df75ac0693e926
jonathan <jonathan@calazan.com>
2015-04-19 04:07:53 -0500

    Highlight url on click.

--- scripts/deploy_youtubeadl.sh
+++ scripts/deploy_youtubeadl.sh
@@ -2,4 +2,4 @@
 
 cd ../ansible
 
-ansible-playbook -i production ../ansible/site.yml --tags="deploy" --ask-vault-pass -vvvv
+ansible-playbook -i production ../ansible/site.yml --tags="deploy" --ask-vault-pass
