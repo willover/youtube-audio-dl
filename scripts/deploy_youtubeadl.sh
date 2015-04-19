83732fd994517587a6ca02e68cf98b63f31af082
jonathan <jonathan@calazan.com>
2015-04-19 03:50:59 -0500

    Removed commented out code.

--- scripts/deploy_youtubeadl.sh
+++ scripts/deploy_youtubeadl.sh
@@ -2,5 +2,4 @@
 
 cd ../ansible
 
-#ansible-playbook -i production ../ansible/site.yml --tags="deploy,celery" --ask-vault-pass -vvvv
 ansible-playbook -i production ../ansible/site.yml --ask-vault-pass
